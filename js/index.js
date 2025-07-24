3dbb07ab0df222bfa3c17755ee651fe52a558579
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-07-24 23:06:37 +0500

    small changes in html

--- js/index.js
+++ js/index.js
@@ -10,17 +10,4 @@ function myFun(event){
     result.append(tag);
 
 }
-function myFun(event) {
-    event.preventDefault();
-    var form = document.forms.elementForm;
-    console.log(form.elements);
-    
-    var elementName = form.elements.element.value;
-    var text = form.elements.text.value; // Corrected typo: "elemetns" to "elements"
-    
-    var tag = document.createElement(elementName);
-    tag.innerHTML = text;
-    
-    var result = document.getElementById("result");
-    result.appendChild(tag); // Changed "append" to "appendChild"
- }
+
